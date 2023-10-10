e1927bb0e90f87792dc31f8463a12747fe74953f
chris <chrisa.code@gmail.com>
2023-10-09 19:53:41 -0500

    little tweaks

--- basic.py
+++ basic.py
@@ -1,15 +1,20 @@
 from autogen import AssistantAgent, UserProxyAgent, config_list_from_json
-import openai
 
-#import api key
+# import api key
 config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
 llm_config = {"config_list": config_list, "seed": 42, "request_timeout": 120}
 
-#create user proxy agent
-user_proxy = UserProxyAgent(name="user_proxy", code_execution_config={"work_dir": "coding"})
-#create assistant agent
-assistant = AssistantAgent(name="assistant", llm_config={"model": "gpt-3.5-turbo","config_list": config_list})
+# create user proxy agent
+user_proxy = UserProxyAgent(
+    name="user_proxy", code_execution_config={"work_dir": "autogen-output"}
+)
+# create assistant agent
+assistant = AssistantAgent(
+    name="assistant", llm_config={"model": "gpt-3.5-turbo", "config_list": config_list}
+)
 
 
-#start the conversation
-user_proxy.initiate_chat(assistant, message="Plot a chart of NVDA and TESLA stock price change YTD.")
+# start the conversation
+user_proxy.initiate_chat(
+    assistant, message="Plot a chart of NVDA and TESLA stock price change YTD"
+)
